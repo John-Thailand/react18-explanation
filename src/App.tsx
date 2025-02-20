76888903f7b439487478814a76208dfdb6aefce5
John-Thailand <naoki.honda@mirrorfit.jp>
2025-02-20 19:54:27 +0900

    サンプルアプリの作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { AutoBatchEventHandler } from './components/AutoBatchEventHandler';
 import { AutoBatchOther } from './components/AutoBatchOther';
+import { Transition } from './components/Transition';
 
 function App() {
   useEffect(() => {
@@ -12,6 +13,8 @@ function App() {
     <div className="App">
       <AutoBatchEventHandler />
       <AutoBatchOther />
+      <hr />
+      <Transition />
     </div>
   );
 }
